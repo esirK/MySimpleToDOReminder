fcb5b5bc77ca5d68d3ede0e42d4779b4565746fa
esirK <esirkings@gmail.com>
2017-03-11 04:00:46 +0300

    Day One

--- app/src/main/java/com/janta/esir/mysimpletodoreminder/MainActivity.java
+++ app/src/main/java/com/janta/esir/mysimpletodoreminder/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //My Main Activity Here
+        //My Main Activity Here and There
     }
 }
